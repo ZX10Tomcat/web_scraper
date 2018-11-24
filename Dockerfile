7f30429708bb66178c6016520599dd71819f80a7
Bogdan Samoletskyi <tomcat_iv@yahoo.com>
2018-11-24 22:59:20 +0000

    First commit with working server.js file

---
+++ Dockerfile
@@ -0,0 +1,30 @@
+FROM node:8
+
+# Who(m) to blame if nothing works
+MAINTAINER tomcat_iv@yahoo.com
+
+# Create a working directory 
+RUN mkdir -p /usr/src/app
+ 
+# Switch to working directory
+WORKDIR /usr/src/app
+ 
+# Copy contents of local folder to `WORKDIR`
+# You can pick individual files based on your need
+COPY . .
+ 
+# Install nodemon globally
+RUN npm install -g nodemon
+
+# Install request-promise cheerio and puppeteer
+
+RUN npm install --save request-promise cheerio puppeteer 
+
+# Install dependencies (if any) in package.json
+RUN npm install
+
+# Expose port from container so host can access 3000
+EXPOSE 3000
+ 
+# Start the Node.js app on load
+CMD [ "npm", "start" ]
---
+++ server.js
@@ -0,0 +1,64 @@
+'use strict';
+
+const express = require('express');
+const rp = require('request-promise');
+const url = 'https://www.fia.com/events/fia-formula-one-world-championship/season-2018/session-classifications-18'
+
+const cheerio = require('cheerio');
+// Constants
+const PORT = 3000;
+const HOST = '0.0.0.0';
+
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+  rp(url)
+    .then(function(html) {
+      
+      const $ = cheerio.load(html); 
+      const results = [];
+
+      //Get  all tables
+      let data = $('.external-xml-data').find($('.table')).children();
+      data.each(function(i){        
+        const session = [];  
+        
+        //Get results from table
+        let table = $(this).find($('.competitor'));
+        getTable(table, $, session);
+        
+        results[i] = {header : $(this).find('thead').text().trim(),
+                      results : session};
+      }); 
+
+      res.send(results);
+    }) 
+    .catch(function(err){
+      res.send(err);
+    })
+
+  //res.send('Hello world docker+\n');
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
+
+function getTable(table, $, session) {
+  table.each(function (i) {
+    let result = {};
+
+    //Get result from row
+    let row = $(this).find('td');
+    getRow(row, $, result);
+    session[i] = result;
+  });
+}
+
+function getRow(row, $, result) {
+  row.each(function () {
+    let column = $(this).attr('class');
+    result[column] = $(this).text();
+  });
+}
+
